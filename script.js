4dab6792ff42a90fe95790f4473731dc94da98b7
Joko Sanyang <jo.san@live.co.uk>
2019-02-27 21:00:05 +0000

    Cleaned up code with operator variable, tidied CSS

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // declare variables
 var display = document.getElementById("display");
 var calculator = document.getElementById("calc");
-var numbers = document.getElementsByClassName("number");
-var calcs = document.getElementsByClassName("calculations");
 var result = 0;
+var operator = "";
 
 // make numbers appear in the display
 calculator.addEventListener("click", print);
@@ -20,47 +19,29 @@ calculator.addEventListener("click", print);
 // make calculations work
 calculator.addEventListener("click", calculate);
 
-        var a = null;
-        var b = null;
-        var c = null;
-        var d = null;
-
     function calculate(e) {
-        if (e.target && e.target.id == "plus") {
-            a = parseFloat(display.textContent);
-            display.textContent = 0;
-        }
-        if (e.target && e.target.id == "minus") {
-            b = parseFloat(display.textContent);
-            display.textContent = 0;
-        }
-        if (e.target && e.target.id == "multiply") {
-             c = parseFloat(display.textContent);
-            display.textContent = 0;
-        }
-        if (e.target && e.target.id == "divide") {
-            d = parseFloat(display.textContent);
+        if (e.target && e.target.classList.contains("calculations")) {
+            result = parseFloat(display.textContent);
+            operator = e.target.id;
             display.textContent = 0;
         }
         if (e.target && e.target.id == "equals") {
-            if (a !== null) {
-            result = a + parseFloat(display.textContent);
-            } if (b !== null) {
-            result = b - parseFloat(display.textContent);
-            } if (c !== null) {
-            result = c * parseFloat(display.textContent);
-            } if (d !== null) {
-            result = d / parseFloat(display.textContent);
+            if (operator == "plus") {
+            result = result + parseFloat(display.textContent);
+            } if (operator == "minus") {
+            result = result - parseFloat(display.textContent);
+            } if (operator == "multiply") {
+            result = result * parseFloat(display.textContent);
+            } if (operator == "divide") {
+            result = result / parseFloat(display.textContent);
             }
             display.textContent = result;
             result = 0;
-            a = null;
-            b = null;
-            c = null;
-            d = null;
+            operator = "";
         }
-        if (e.target && e.target.id == "clear") {
+            if (e.target && e.target.id == "clear") {
             result = 0;
             display.textContent = result;
+            operator = "";
         }
     }
